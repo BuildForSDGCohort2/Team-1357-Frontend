c1ec7ffb43e4864a587b59c2a487d0373c3ce16e
John Kyony <jkyony@gmail.com>
2020-09-05 21:00:15 +0200

    feat: event organiser can view all event team members

---
+++ event-organiser-app/src/components/TeamMembers.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+            <div class="card">
+                <img class="card-img-top" src="https://picsum.photos/200/150/?random">
+                <div class="card-block">
+                    <h5 class="text-bold" id="teamMemberName">{{teamMembersProfile}}</h5>
+                </div>
+            </div>
+        </div>
+       
+       
+    </div>
+    
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            teamMembersProfile : {
+                emailAddress: 'abc@gmail.com',
+                avatar: 'https://dekhnews.com/wp-content/uploads/2016/04/Sports-Day-Poster.png',
+                name: 'abcde',
+                id: 2
+
+            },
+        }
+    }
+}
+</script>
